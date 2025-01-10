829e48ec39eeafcd48b008e2b1a49c8ae01521fa
Piyush-daryapurkar <piyushdaryapurkar309@gmail.com>
2025-01-10 23:32:03 +0530

    stack

---
+++ stack/aaaaaaaaaaaaaaaaaaa.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	
+//	stack:~ implimantaction with the help of array linkedlist
+//  stack:~FILO/LIFO
+
+//	1.push(insert)
+//	2.pop(del)
+//	3.peek(travers at top)
+//	4.isfull()
+//	5.isempty
+
+//    push(insert):~insert at allways on top 
+
+//    if stack[top]=n-1 = array is full(we can put this condition at start of code)
+
+//     algoritham of stack:~
+//                         1. check stack id s full
+//                         2. if full take exit 
+//                         3. if not full than prisid insert new element the pos of top
+//                         4. top++
+
+
+	return 0;
+}
