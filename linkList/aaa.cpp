3cc12c3c98506e8e63a441d0fc124bc9d7cb2ab3
Piyush-daryapurkar <piyushdaryapurkar309@gmail.com>
2025-01-13 22:56:22 +0530

    linked

---
+++ linkList/aaa.cpp
@@ -0,0 +1,7 @@
+//linkedList :~
+             // 1. singley linklist
+             // 2. dubley linkedlist
+             // 3. circular linkedlist :~  // <I> singly circular
+                                          //  <II> dublily linklist
+
+             //
