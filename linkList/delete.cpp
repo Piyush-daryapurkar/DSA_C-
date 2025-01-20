d0546a6e31ab643384d5ce9a589a23974e36ea9c
Piyush-daryapurkar <piyushdaryapurkar309@gmail.com>
2025-01-20 23:37:26 +0530

    delete

---
+++ linkList/delete.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+using namespace std;
+struct Node {
+	int data;
+	Node *next;
+};
+Node *head  = NULL;
+
+void insert(int x)
+{
+	Node *newNode = new Node;
+	newNode->data = x;
+	newNode->next = head;
+	head = newNode;
+}
+
+void delNode()
+{
+	if(head==NULL)
+	{
+		cout<<"list is empty"<<endl;
+	}
+	Node *temp=head;
+	head=temp->next;
+	delete temp;
+}
+
+void delNodeAE()
+{
+	if(head==NULL)
+	{
+		cout<<"list is empty"<<endl;
+	}
+	if(head->next==NULL)
+	{
+		head=head->next;
+		delete head;
+		
+	}
+	
+	Node *temp=head;
+	while(temp->next != NULL)
+	{
+		temp=temp->next;
+	}
+	delete temp->next;
+	temp->next=NULL;
+}
+void show()
+{
+	Node *temp = head;
+	while(temp != NULL)
+	{
+		cout<<temp->data<<" ";
+		temp = temp->next ;
+	}
+	cout<<endl<<endl;
+}
+int main ()
+{
+	insert(10);
+	insert(11);
+	insert(12);
+	insert(14);
+	show();
+	delNode();
+	show();
+	delNodeAE();
+	show();
+	delNodeAE();
+	delNodeAE();
+	delNodeAE();
+	show();
+
+
+
+	return 0;
+}
+
